473364f0a398934c9945dea5fe710f42232c33ce
mook <mookjpy@gmail.com>
2016-01-25 01:43:18 +0900

    Fix logger

--- main.rb
+++ main.rb
@@ -9,55 +9,51 @@ dog = Dogapi::Client.new(API_KEY, APP_KEY)
 
 PIN_NUMBER = 2
 
-Class SimpleLogger
+class SimpleLogger
   MODE = { :DETAIL => 'DETAIL', :SUMMARY => 'SUMMARY' }
-
-  def new(mode)
-    self.mode = mode
-    self.switch_count = 0
-    self.on_count = 0
-    self.off_count = 0
+  
+  @mode
+  @switch_count
+  @on_count
+  @off_count
+  
+  def initialize(mode)
+    @mode = mode
+    @switch_count = 0
+    @on_count = 0
+    @off_count = 0
   end
-
+  
   def log(message)
-    if mode == MODE[:DETAIL]
+    if @mode == MODE[:DETAIL]
       log_detail(message)
-      return
+    else
+      log_summary(message)
     end
-    log_summary(message)
   end
-
+  
   def log_detail(message)
     pp "[#{Time.now}] - #{message}"
   end
-
+  
   def log_summary(message)
-    self.switch_count += 1
-    
+    @switch_count += 1
+  
     if message == 'on'
-      self.on_count += 1
+      @on_count += 1
     else
-      self.off_count += 1
+      @off_count += 1
     end
-    
-    if self.switch_count > 1000
-      pp "[#{Time.now}] - on_count: #{self.on_count} off_count: #{self.off_count}"
-      self.switch_count = 0
-      self.on_count = 0
-      self.off_count = 0
+  
+    if @switch_count > 1000
+      pp "[#{Time.now}] - on_count: #{@on_count} off_count: #{@off_count}"
+      @switch_count = 0
+      @on_count = 0
+      @off_count = 0
     end
   end
 end
 
-def on(io, pin_number)
-  io.digital_write(pin_number, WiringPi::HIGH)
-  logger.log('on')
-end
-
-def off(io, pin_number)
-  io.digital_write(pin_number, WiringPi::LOW)
-  logger.log('off')
-end
 
 io = WiringPi::GPIO.new do |gpio|
   gpio.pin_mode(2, WiringPi::OUTPUT)
@@ -72,9 +68,11 @@ loop do
   end.size
 
   if alart_num > 0
-    on(io, PIN_NUMBER)
+    io.digital_write(PIN_NUMBER, WiringPi::HIGH)
+    logger.log('on')
   else
-    off(io, PIN_NUMBER)
+    io.digital_write(PIN_NUMBER, WiringPi::LOW)
+    logger.log('off')
   end
 
   sleep 5
